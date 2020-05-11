5ea1cd360d5cbc4e636ca25370c32460150a30a3
PhongNguyenVan <phongnv@linxhq.com>
2020-05-11 12:07:05 +0700

    add migration

---
+++ linxOne-Net/linxOne.Data/EF/linxOneDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace linxOne.Data.EF
+{
+  public  class linxOneDbContextFactory : IDesignTimeDbContextFactory<linxOneDbContext>
+    {
+        public linxOneDbContext CreateDbContext(string[] args)
+        {
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json")
+              .Build();
+            // ....................
+
+
+
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<linxOneDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=blog.db");
+
+            return new linxOneDbContext(optionsBuilder.Options);
+        }
+    }
+} 
