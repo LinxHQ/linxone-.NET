82a4f5e323b30d5a4c76169be255badace51ffc1
PhongNguyenVan <phongnv@linxhq.com>
2020-05-12 11:47:32 +0700

    data seeding, install data default for entity

---
+++ linxOne-Net/linxOne.Data/Migrations/20200512044515_SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace linxOne.Data.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Ib_taxes",
+                columns: new[] { "Ib_record_primary_key", "Ib_tax_name", "Ib_tax_value" },
+                values: new object[,]
+                {
+                    { 1, "FL", "6.50" },
+                    { 2, "VAT", "14.00" },
+                    { 3, "TGST", "5.00" },
+                    { 4, "GST%", "5.00" },
+                    { 5, "GST 17%", "5.00" },
+                    { 6, "IVA2", "16.00" },
+                    { 7, "IVA", "13.00" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Ib_taxes",
+                keyColumn: "Ib_record_primary_key",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Ib_taxes",
+                keyColumn: "Ib_record_primary_key",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Ib_taxes",
+                keyColumn: "Ib_record_primary_key",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Ib_taxes",
+                keyColumn: "Ib_record_primary_key",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Ib_taxes",
+                keyColumn: "Ib_record_primary_key",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Ib_taxes",
+                keyColumn: "Ib_record_primary_key",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Ib_taxes",
+                keyColumn: "Ib_record_primary_key",
+                keyValue: 7);
+        }
+    }
+}
